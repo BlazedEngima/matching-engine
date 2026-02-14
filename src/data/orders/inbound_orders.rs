afefcf9d3046dabdcda0f698b146923b860b2a18
Samuel Theofie <samueltheofie@gmail.com>
2026-02-14 19:47:35 +0000

    Chore: remove dead_code markers

--- src/data/orders/inbound_orders.rs
+++ src/data/orders/inbound_orders.rs
@@ -1,6 +1,5 @@
 use crate::data::order_types::IncomingSide;
 
-#[allow(dead_code)]
 pub struct IncomingLimitOrder {
     pub order_id: u64, // u64 for simplicity. Probably use UUID in real scenarios.
     pub price: u64,
@@ -8,14 +7,12 @@ pub struct IncomingLimitOrder {
     pub side: IncomingSide,
 }
 
-#[allow(dead_code)]
 pub struct IncomingMarketOrder {
     pub order_id: u64,
     pub qty: u32,
     pub side: IncomingSide,
 }
 
-#[allow(dead_code)]
 pub struct IncomingCancelOrder {
     pub order_id: u64,
 }
