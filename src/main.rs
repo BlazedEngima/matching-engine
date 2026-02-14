943d3e6165813a579d762891ff0bfd7cf97642ed
Samuel Theofie <samueltheofie@gmail.com>
2026-02-14 23:16:02 +0000

    Feat: init main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,96 @@
-use matching_engine::{data::fill_type::BookEvent, engine::matching_engine::Engine};
-use rtrb::RingBuffer;
+use clap::Parser;
+use matching_engine::data::book_event::BookEvent;
+use matching_engine::data::order_types::IncomingOrder;
+use matching_engine::engine::matching_engine::Engine;
+use matching_engine::input::generator::Generator;
+use matching_engine::input::replay_reader::ReplayReader;
+use matching_engine::logger::book_logger::BookLogger;
+use rtrb::{PopError, RingBuffer};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
 
-fn main() {
-    // Initialize SPSC ring_buffer to communicate to logging thread
-    let (mut prod, mut cons) = RingBuffer::<BookEvent>::new(1 << 16);
-    let engine = Engine::new(1 << 16);
+#[derive(Parser, Debug)]
+struct Args {
+    /// gen or replay
+    #[arg(long)]
+    mode: String,
 
-    // Logging thread
-    thread::spawn(move || {
-        while let Ok(event) = cons.pop() {
-            log_event(event);
+    #[arg(long, default_value = "replay_output.csv")]
+    replay_output: String,
+
+    /// Input file for replay mode
+    #[arg(long)]
+    input: Option<String>,
+
+    /// Output file
+    #[arg(long, default_value = "output.log")]
+    output: String,
+}
+
+const DEFAULT_SIZE: usize = 1 << 16;
+const NUM_OF_EVENTS: usize = 10_000;
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    // Input generation of orders
+    let input_events: Vec<IncomingOrder> = match args.mode.as_str() {
+        "gen" => {
+            println!("Generating random input...");
+            let mut generator = Generator::new(12345, 10_000, &args.replay_output)?;
+            generator.generate(NUM_OF_EVENTS) // generate N events
         }
+        "replay" => {
+            println!("Loading replay file...");
+            let input = args.input.expect("Replay mode requires --input <file>");
+            let mut reader = ReplayReader::from_file(input.as_str())?;
+            reader.parse_orders()
+        }
+        _ => panic!("Invalid mode. Use --mode gen or --mode replay"),
+    };
+
+    println!("Loaded {} input events", input_events.len());
+
+    // Init ring buffer and syncing atmoic bool
+    let (mut producer, mut consumer) = RingBuffer::<BookEvent>::new(DEFAULT_SIZE);
+    let done = Arc::new(AtomicBool::new(false));
+    let done_producer = done.clone();
+    let output_path = args.output;
+
+    // Spawn logging thread
+    let logger_handle = thread::spawn(move || -> anyhow::Result<()> {
+        let mut logger = BookLogger::new(&output_path)?;
+        while !done.load(Ordering::Relaxed) && consumer.is_empty() {
+            match consumer.pop() {
+                Ok(event) => logger.log(&event)?,
+                Err(PopError::Empty) => std::hint::spin_loop(),
+            }
+        }
+
+        logger.flush()?;
+        Ok(())
     });
 
-    // Matching engine thread
-    prod.push(event).ok(); // ideally avoid blocking
+    let mut engine = Engine::new(1 << 16);
+
+    // Perform main engine matching task
+    for order in input_events {
+        let events = engine.match_order(order);
+        for event in events {
+            producer.push(event)?;
+        }
+    }
+
+    done_producer.store(true, Ordering::Relaxed);
+
+    // --------------------------------------------------
+    // Step 5: Wait for Logger Thread
+    // --------------------------------------------------
+
+    logger_handle.join().unwrap()?;
+
+    println!("Done.");
+
+    Ok(())
 }
