d79a40b5463b7eeb4cc16f45326565e495c134eb
Samuel Theofie <samueltheofie@gmail.com>
2026-02-14 19:33:11 +0000

    Feat: updated fill type for more logging/journaling types

--- src/data/fill_type.rs
+++ src/data/fill_type.rs
@@ -1,8 +1,27 @@
 use crate::data::orders::resting_orders::OrderId;
 
-pub struct FillEvent {
+pub enum FillEvent {
+    Match(MatchEvent),
+    Cancel(CancelEvent),
+    Insert(InsertEvent),
+}
+
+pub struct MatchEvent {
     pub maker: OrderId,
     pub taker: OrderId,
+    pub price: u64,
+    pub qty: u32,
+    pub ts: i64,
+}
+
+pub struct CancelEvent {
+    pub order_id: OrderId,
+    pub qty: u32,
+    pub ts: i64,
+}
+
+pub struct InsertEvent {
+    pub order_id: OrderId,
     pub price: u64,
     pub qty: u32,
     pub ts: i64,
